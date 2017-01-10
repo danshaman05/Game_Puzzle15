eaebe379a0939a1a78349c11d6820f40dc83716b
danshaman05 <dany05@email.cz>
2017-01-10 16:49:09 +0100

    nahodne obrazky a nahodne levely

--- puzzle.py
+++ puzzle.py
@@ -10,7 +10,7 @@ import time
 class Plocha:
 
 ##BUGY:
-    #Load game nech nahra aj obrazok konkretny!
+#Load game nech nahra aj obrazok konkretny!
 
 
 ##DOROBIT:
@@ -31,18 +31,13 @@ class Plocha:
 ## ak 2x kliknem na stvorcek, kt. sa da hybat, tak mi ho vysunie prec a prekryje iny stvorcek alebo vyjde mimo plochu
 # chybna hlaska na konci hry - unbind - deletecommand() argument must be str, not method
 
-##Moje pozn.:
-# itemconfig nesluzi na presun zmenu suradnic utvaru!
-##time.sleep() nie je dobre pouzit na kratke zastavenie programu
-   
+ 
 ################ __init__ ##############################
 
     def __init__(self, jpg_subor):
         self.canvas = tkinter.Canvas(width=600, height=100*4+50, bg='white')
         self.canvas.pack()
-
         Stvorec.canvas = self.canvas
-
         
         # NACITA OBR:
         self.obrazok_original = Image.open(jpg_subor)
@@ -72,19 +67,17 @@ class Plocha:
             for j in range(4):
                 riadok.append([i,j])
             self.indexy.append(riadok)
-
-              
-        #INDEXY_riesenie (kvoli overeniu vitazstva): 
-        self.indexy_riesenie = [x[:] for x in self.indexy] #OK
+    
+        #INDEXY_riesenie (kopia self.indey - kvoli overeniu vitazstva): 
+        self.indexy_riesenie = [x[:] for x in self.indexy]
         
         self.poc_tahov = 0
-
         self.pridaj_stvorce()
-
         self.matfyzak = None
-        
         self.new_game()
-        
+        self.poz_nuly = list(self.nula())
+        self.info_save_game = None
+        self.napoveda()
 
 # ######  PRE KLAVESY:
 ##        self.canvas.bind_all('<Up>', self.posun_hore)
@@ -93,24 +86,16 @@ class Plocha:
 ##        self.canvas.bind_all('<Left>', self.posun_vlavo)
 
 
-        self.poz_nuly = list(self.nula())
-
         #Nastavenie rychlosti posunu stvorcov - DOROBIT
 ##        self.rychlo = False
 ##        Stvorec.rychlo = self.rychlo
 
-        self.info_save_game = None
-
-        self.napoveda()
-
-
+# VLASTNY TEST:
     def print_indexy(self):
         print()
         print('self.indexy: ')
         for i in self.indexy:
-            print(i)
-
-        
+            print(i)      
 
 ################ KONIEC __init__ ####################
 
@@ -144,8 +129,6 @@ class Plocha:
         #textove info naspodku o ulozeni hry:
         self.info_save_game = self.canvas.create_text(110,420, text='Hra bola ulozena do rozohrata.txt')
 
-
-
     def load_game(self):
         if self.newgame == True: #ak sa zacina new game
             filename = 'new_game/game1.txt'
@@ -154,10 +137,7 @@ class Plocha:
             self.indexy = []
         
         self.indexy = json.load(open(filename))
-##        print('printujem self.indexy: ', self.indexy)
         print('nahral som subor: ', filename)
-        self.print_indexy()
-##        print('self.indexy su typu: ', type(self.indexy))
         
         self.canvas.after(500)
         self.canvas.update()
@@ -172,7 +152,7 @@ class Plocha:
         for i in range(len(self.indexy)):
             if self.indexy[i] != self.indexy_riesenie[i]:
                 return False
-        self.stvorce[-1][-1].unhide() ################ DOROBIT!
+        self.stvorce[-1][-1].unhide()
         self.matfyzak = self.canvas.create_text(300,200, font='arial 40 bold',text='Si super Matfyzák!', fill='gold')
         
         self.canvas.unbind('<Button-1>')
@@ -181,7 +161,6 @@ class Plocha:
 
     def pridaj_stvorce(self):
         '''pridava zaradom stvorce kazdy podla mriezky... To, kt. stvorec sa prida, je urcene v self.indexy'''
-        ...
         y = 0
         for i in range(4):
             x =0
@@ -202,21 +181,15 @@ class Plocha:
             for j in range(4):
                 x, y = self.indexy[i][j]
                 self.stvorce[x][y].move_to(j*100+10, i*100+10)
-
-##                self.canvas.after(105)
                 self.canvas.after(70)
                 self.canvas.update()
 
         self.poz_nuly = list(self.nula())
         
-                   
-
-
-    def pocet_tahov(self): #ZMENIT NA RETURN! a pridat label
+    def pocet_tahov(self): #zmenit na return a pridat label
         self.poc_tahov += 1
         self.canvas.itemconfig(self.pocitadlo, text='pocet tahov: '+str(self.poc_tahov))
-        
-        
+         
     def nula(self):
 ##        if self.poc_tahov > 0:
 ##            return list(self.poz_nuly)
@@ -257,20 +230,17 @@ class Plocha:
 
         return self.dic
 
-    def mouse_click(self, event): #stara vymen_obr 
-        
+    def mouse_click(self, event):       
         dic = self.volne()
-        
         if 0 <= event.x <= 400 and 0 <= event.y <= 400 and self.mozes_tah == True:
 
-            self.print_indexy()
-            print('DIC:', self.volne())
+            print('volne policka a smer kde sa daju hybat:')
+            print(self.volne())
             j = event.x // 100 #stlpec
             i = event.y // 100 #riadok
             
             
             if (i,j) in dic:
-
                 a = int(dic[(i,j)][1] / 100)
                 b = int(dic[(i,j)][0] / 100)
                 
@@ -282,29 +252,23 @@ class Plocha:
                 nula1 = self.indexy[pozicia_nuly[0]][pozicia_nuly[1]][0] #indexy nuly
                 nula2 = self.indexy[pozicia_nuly[0]][pozicia_nuly[1]][1]
 
-
                 Stvorec.move(self.stvorce[kliknute1][kliknute2], *dic[(i,j)])
                 Stvorec.move(self.stvorce[nula1][nula2], -(dic[(i,j)][0]), -(dic[(i,j)][1]))
-
-##                print('suradnice nulu kde posunut', -(dic[(i,j)][0]), -(dic[(i,j)][1]))
 
                 self.poz_nuly[0] -= int(dic[(i,j)][1] / 100)
                 self.poz_nuly[1] -= int(dic[(i,j)][0] / 100)  
               
                 self.indexy[pozicia_nuly[0]][pozicia_nuly[1]] = [ self.indexy[i][j][0], self.indexy[i][j][1] ] #volne_policko[1], volne_policko[0] ########
                 self.indexy[i][j] = [3,3]
-
+                
                 self.pocet_tahov()
-
                 self.zisti_vyhru()
-
                 self.canvas.delete(self.info_save_game)
-
                 self.newgame == False
             
 
 
-#PRE Klavesy: 
+#PRE Klavesy: - dorobit
 ##    def posun_dole(self, event):
 ##        if 'd' in self.volne():
 ##            self.vymen_obr('d')            
@@ -334,8 +298,6 @@ class Plocha:
 ##            self.poz_nuly[1] += 1
 ##            self.volne()
 
-
-
     def napoveda(self):
         self.napoveda_img = self.obrazok_original.resize((120, 120)) #napoveda = maly obrazok vpravo
         prazdny = Image.new('RGB', (30, 30), 'white')
@@ -350,12 +312,8 @@ class Stvorec:
         self.x, self.y = x, y
         if image_zdroj != 0:
             self.id = self.canvas.create_image(x, y, image=image_zdroj, anchor='nw') #outline tu nefunguje!
-##            canvas.create_text(x+50, y+50, text=)
-
         self.mozes_tah = True
         Plocha.mozes_tah = self.mozes_tah
-
-##        Plocha.mozes_tah = self.mozes_tah
 
     def move(self, x, y):
         Plocha.mozes_tah = False
@@ -367,15 +325,8 @@ class Stvorec:
             self.canvas.move(self.id, x/self.cas, y/self.cas)
             self.canvas.after(10)
             self.canvas.update()
-
-##        print('self.mozes_tah: ', self.mozes_tah)
         Plocha.mozes_tah = True
         
-            
-            
-##            self.load()
-
-##        self.canvas.after(200)
 
     def move_to(self, x, y):
         self.canvas.coords(self.id, x, y)
@@ -384,12 +335,7 @@ class Stvorec:
         self.canvas.itemconfig(self.id, state='hidden')
         
     def unhide(self): 
-        self.canvas.itemconfig(self.id, state='')
-        
-
-##    def vnutri(self, x, y):
-##        return abs(self.x-x) <= 100 and abs(self.y-y) <= 100
-        
+        self.canvas.itemconfig(self.id, state='')     
             
 
 ######################################################### PROGRAM ##############
@@ -397,18 +343,8 @@ class Program:
     def __init__(self):
 
         self.okno = tkinter.Tk() # vytv. graf. okno
-
-        self.plocha = Plocha('obr/earth.jpg')
-        self.menu()
-
-        # NAPOVEDA napravo:
-        
-
-        print()
-        print('DIC:', self.plocha.volne())
-        print()
- 
-        
+        self.plocha = Plocha('obr/earth.jpg') #zatial iba tu sa meni obr
+        self.menu()      
         tkinter.mainloop()
 
 
